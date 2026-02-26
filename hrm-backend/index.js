9a14f8811630b6a49f446552a27a45067d00dfef
majidkhan0786 <majid@zmediostech.com>
2026-02-27 04:09:10 +0530

    change base url

--- hrm-backend/index.js
+++ hrm-backend/index.js
@@ -11,10 +11,7 @@ const app = express();
 /* -------- Middlewares -------- */
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://hr-management-system-j91g.vercel.app"
-    ],
+    origin: "*", // For public access (change to specific domain for security)
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
